8affbab04ad67bcc5aef72d855628e4407b47628
mreitenb <48971407+mreitenb@users.noreply.github.com>
2021-05-29 01:58:08 -0600

    Delete hashes.py

--- hashes.py
+++
@@ -1,9 +0,0 @@
-from hashlib import sha256
-
-def sha_256_hex(string):
-    # Returns the SHA-256 hash of a string as a hexadecimal string  
-    return sha256(string.encode()).hexdigest()
-
-def sha_256(string):
-    # Returns the SHA-256 hash of a string as a base 10 integer
-    return int(sha_256_hex(string), 16)
